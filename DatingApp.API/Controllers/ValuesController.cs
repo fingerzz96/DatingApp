0ccef46d9d7a8349dd679fa9f93405904e60eaa6
fingerzz <slavkovic.lazar96@gmail.com>
2019-07-27 21:08:03 +0200

    Creating authentication for app, DTO's and validation

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -3,20 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DatingApp.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         private readonly DataContext _dataContext;
 
-        public ValuesController(DataContext dataContext)
-        {
-            _dataContext = dataContext;
-        }
+        public ValuesController(DataContext dataContext) { _dataContext = dataContext; }
+
         // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
@@ -27,6 +27,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public ActionResult<string> GetValue(int id)
         {
@@ -36,21 +37,12 @@ namespace DatingApp.API.Controllers
         }
 
         // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+        [HttpPost] public void Post([FromBody] string value) { }
 
         // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+        [HttpPut("{id}")] public void Put(int id, [FromBody] string value) { }
 
         // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        [HttpDelete("{id}")] public void Delete(int id) { }
     }
 }
