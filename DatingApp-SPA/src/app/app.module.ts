cfae48b9fa08b40699614a1b89a068ec65dfc063
Lazar Slavkovic-Raco <slavkovic.lazar96@gmail.com>
2019-09-21 06:18:05 +0200

    Building User Interface

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,8 @@
-import {BrowserModule} from '@angular/platform-browser';
+import {BrowserModule, HAMMER_GESTURE_CONFIG, HammerGestureConfig} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import {JwtModule} from '@auth0/angular-jwt';
 
 import {AppComponent} from './app.component';
 import {NavComponent} from './nav/nav.component';
@@ -10,13 +11,29 @@ import {HomeComponent} from './home/home.component';
 import {RegisterComponent} from './register/register.component';
 import {ErrorInterceptorProvider} from './_services/error.interceptor';
 import {AlertifyService} from './_services/alertify.service';
-import {BsDropdownModule} from 'ngx-bootstrap';
-import {MemberListComponent} from './member-list/member-list.component';
+import {BsDropdownModule, TabsModule} from 'ngx-bootstrap';
+import {MemberListComponent} from './members/member-list/member-list.component';
 import {ListsComponent} from './lists/lists.component';
 import {MessagesComponent} from './messages/messages.component';
 import {RouterModule} from '@angular/router';
 import {appRoutes} from './routes';
 import {AuthGuard} from './_guards/auth.guard';
+import {MemberCardComponent} from './members/member-card/member-card.component';
+import {MemberDetailComponent} from './members/member-detail/member-detail.component';
+import {MemberDetailResolver} from './_resolver/member-detail.resolver';
+import {MemberListResolver} from './_resolver/member-list.resolver';
+import {NgxGalleryModule} from 'ngx-gallery';
+
+export function tokenGetter() {
+  return localStorage.getItem('token');
+}
+
+export class CustomHammerConfig extends HammerGestureConfig {
+  overrides = {
+    pinch: {enable: false},
+    rotate: {enable: false}
+  };
+}
 
 @NgModule({
   declarations: [
@@ -25,23 +42,38 @@ import {AuthGuard} from './_guards/auth.guard';
     HomeComponent,
     RegisterComponent,
     MemberListComponent,
+    MemberDetailComponent,
     ListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
+    NgxGalleryModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    TabsModule.forRoot(),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ['localhost:5000'],
+        blacklistedRoutes: ['localhost:5000/api/auth']
+      }
+    })
   ],
   providers: [
     AuthService,
     ErrorInterceptorProvider,
     AlertifyService,
-    AuthGuard
+    AuthGuard,
+    MemberDetailResolver,
+    MemberListResolver,
+    {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}
   ],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
