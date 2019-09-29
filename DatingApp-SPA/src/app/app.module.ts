83f5eb262dc24451c1f34c0c81b4a16eadf732ce
Lazar Slavkovic-Raco <slavkovic.lazar96@gmail.com>
2019-09-29 16:28:49 +0200

    Updated resources, members, routes, Dtos

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -23,6 +23,9 @@ import {MemberDetailComponent} from './members/member-detail/member-detail.compo
 import {MemberDetailResolver} from './_resolver/member-detail.resolver';
 import {MemberListResolver} from './_resolver/member-list.resolver';
 import {NgxGalleryModule} from 'ngx-gallery';
+import {MemberEditComponent} from './members/member-edit/member-edit.component';
+import {MemberEditResolver} from './_resolver/member-edit.resolver';
+import {PreventUnsavedChanges} from './_guards/prevent-unsaved-changes.guard';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -45,7 +48,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     MemberDetailComponent,
     ListsComponent,
     MessagesComponent,
-    MemberCardComponent
+    MemberCardComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -68,8 +72,10 @@ export class CustomHammerConfig extends HammerGestureConfig {
     ErrorInterceptorProvider,
     AlertifyService,
     AuthGuard,
+    PreventUnsavedChanges,
     MemberDetailResolver,
     MemberListResolver,
+    MemberEditResolver,
     {provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig}
   ],
   bootstrap: [AppComponent]
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -1,15 +1,18 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { MemberListComponent } from './members/member-list/member-list.component';
-import { MessagesComponent } from './messages/messages.component';
-import { ListsComponent } from './lists/lists.component';
-import { AuthGuard } from './_guards/auth.guard';
-import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { MemberDetailResolver } from './_resolver/member-detail.resolver';
-import { MemberListResolver } from './_resolver/member-list.resolver';
+import {Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {MemberListComponent} from './members/member-list/member-list.component';
+import {MessagesComponent} from './messages/messages.component';
+import {ListsComponent} from './lists/lists.component';
+import {AuthGuard} from './_guards/auth.guard';
+import {MemberDetailComponent} from './members/member-detail/member-detail.component';
+import {MemberDetailResolver} from './_resolver/member-detail.resolver';
+import {MemberListResolver} from './_resolver/member-list.resolver';
+import {MemberEditComponent} from './members/member-edit/member-edit.component';
+import {MemberEditResolver} from './_resolver/member-edit.resolver';
+import {PreventUnsavedChanges} from './_guards/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
+  {path: '', component: HomeComponent},
   {
     path: '',
     runGuardsAndResolvers: 'always',
@@ -18,16 +21,22 @@ export const appRoutes: Routes = [
       {
         path: 'members',
         component: MemberListComponent,
-        resolve: { users: MemberListResolver }
+        resolve: {users: MemberListResolver}
       },
       {
         path: 'members/:id',
         component: MemberDetailComponent,
-        resolve: { user: MemberDetailResolver }
+        resolve: {user: MemberDetailResolver}
       },
-      { path: 'messages', component: MessagesComponent },
-      { path: 'lists', component: ListsComponent }
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        resolve: {user: MemberEditResolver},
+        canDeactivate: [PreventUnsavedChanges]
+      },
+      {path: 'messages', component: MessagesComponent},
+      {path: 'lists', component: ListsComponent}
     ]
   },
-  { path: '**', redirectTo: '', pathMatch: 'full' }
+  {path: '**', redirectTo: '', pathMatch: 'full'}
 ];
