90a95dfe3960476c40cb39ac03af3c5d8ff429f6
Lazar Slavkovic-Raco <slavkovic.lazar96@gmail.com>
2019-10-30 22:15:25 +0100

    Added 3rd party Photo uploader

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -27,6 +27,7 @@ import {MemberEditComponent} from './members/member-edit/member-edit.component';
 import {MemberEditResolver} from './_resolver/member-edit.resolver';
 import {PreventUnsavedChanges} from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
+import {FileUploadModule} from 'ng2-file-upload';
 
 export function tokenGetter() {
   return localStorage.getItem('token');
@@ -61,6 +62,7 @@ export class CustomHammerConfig extends HammerGestureConfig {
     BsDropdownModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     TabsModule.forRoot(),
+    FileUploadModule,
     JwtModule.forRoot({
       config: {
         tokenGetter: tokenGetter,
--- DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/photo-editor/photo-editor.component.ts
@@ -1,5 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Photo} from '../../_models/photo';
+import {FileUploader} from 'ng2-file-upload';
+import {environment} from '../../../environments/environment';
+import {AuthService} from '../../_services/auth.service';
 
 @Component({
   selector: 'app-photo-editor',
@@ -7,10 +10,33 @@ import {Photo} from '../../_models/photo';
   styleUrls: ['./photo-editor.component.css']
 })
 export class PhotoEditorComponent implements OnInit {
-@Input() photos : Photo[];
-  constructor() { }
+  @Input() photos: Photo[];
+
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean;
+  baseUrl = environment.apiUrl
+
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit() {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader(){
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodedToken.nameid + '/photos',
+      authToken: 'Bearer' + localStorage.getItem('token'),
+      isHTML5 : true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    })
   }
 
 }
