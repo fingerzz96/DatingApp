196971ee0232d6110d84d143236c746ccefcc763
Lazar Slavkovic-Raco <slavkovic.lazar96@gmail.com>
2019-11-14 22:41:17 +0100

    Added filtering in API

--- DatingApp.API/Helpers/UserParams.cs
+++ DatingApp.API/Helpers/UserParams.cs
@@ -6,5 +6,7 @@ namespace DatingApp.API.Helpers
         private int _pageSize = 10;
         public int PageNumber { get; set; } = 1;
         public int PageSize { get => _pageSize; set => _pageSize = (value > _maxPageSize) ? _maxPageSize : value; }
+        public int UserId { get; set; }
+        public string Gender { get; set; }
     }
 }
